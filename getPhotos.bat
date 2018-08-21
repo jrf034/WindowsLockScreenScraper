f289f3f409ddcf7aa45d01d3d7e1adf6d33ab8a4
jrf034 <jrf034@bucknell.edu>
2018-08-21 11:54:54 -0400

    Delete getPhotos.bat

--- getPhotos.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-
-robocopy "C:\Users\%user%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "C:\Users\%user%\Pictures\wallpapers" /s /min:100000
-
-call "rename.bat"
